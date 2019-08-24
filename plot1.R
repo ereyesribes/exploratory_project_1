4067e3773b381fd7479579f504d9a6d4ae6d488f
Hel <jreyesribes@gmail.com>
2019-08-24 11:05:42 +0200

    Cleaner plot 1 code.

--- plot1.R
+++ plot1.R
@@ -6,7 +6,7 @@ total_per_year <- tapply(as.numeric(dataset$Emissions), dataset$year, sum)
 png('plot1.png')
 
 # Make the total PM25 plot
-plot(names(t), t, main = "Particulate Matter 2.5 throughout the years", ylab = "Total Emmissions", xlab = "Year")
-segments(as.numeric(names(t)[1:3]), t[1:3], as.numeric(names(t)[2:4]), t[2:4])
+plot(as.numeric(names(t)), t, main = "Particulate Matter 2.5 throughout the years", ylab = "Total Emmissions", xlab = "Year")
+segments(as.numeric(names(t)[-nrow(t)]), t[-nrow(t)], as.numeric(names(t)[-1]), t[-1])
 
 dev.off()
